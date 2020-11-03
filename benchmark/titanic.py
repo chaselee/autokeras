39df3ffd6c3d1613ac2afc918080b44510045ebb
Haifeng Jin <jin@tamu.edu>
2020-11-03 21:04:37 +0000

    change benchmark code for dashboard

--- benchmark/titanic.py
+++ benchmark/titanic.py
@@ -1,3 +1,6 @@
+import shutil
+from datetime import date
+import sys
 import timeit
 
 import tensorflow as tf
@@ -8,19 +11,26 @@ TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
 TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
 
 
-def main():
+def main(output_file_path):
     train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
     test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
-    clf = ak.StructuredDataClassifier(max_trials=100, directory='tmp_dir', overwrite=True)
+    clf = ak.StructuredDataClassifier(max_trials=1, directory='tmp_dir', overwrite=True)
 
     start_time = timeit.default_timer()
-    clf.fit(train_file_path, 'survived')
+    clf.fit(train_file_path, 'survived', epochs=1)
     stop_time = timeit.default_timer()
 
     accuracy = clf.evaluate(test_file_path, 'survived')[1]
-    print('Accuracy: {accuracy}%'.format(accuracy=round(accuracy * 100, 2)))
-    print('Total time: {time} seconds.'.format(time=round(stop_time - start_time, 2)))
+    total_time = stop_time - start_time
+
+    output = '{today},{accuracy},{total_time}\n'.format(today=date.today().strftime("%m/%d/%y"), accuracy=accuracy, total_time=total_time)
+
+    output_file = open(output_file_path, "a")  # append mode
+    output_file.write(output)
+    output_file.close()
+
+    shutil.rmtree('tmp_dir')
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
